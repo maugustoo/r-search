1da79f4b56812f84fc66c902adecea6699220784
Marcelo Augusto <mekmay@hotmail.com>
2018-02-15 15:09:45 -0200

    Adding state for search bar.

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -3,6 +3,12 @@ import React, { Component } from 'react';
 // const Component = React.Component;
 
 class SearchBar  extends Component{
+    constructor(props) {
+        super(props);
+
+        this.state = { term: ''};
+    }
+
     render() {
         return <input  onChange={this.onInputChange} />;
         // Use method or arrow function like this:
